701d49b7340b511885c9375ffa4e48dc842069d7
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2025-04-06 17:31:02 +0530

    Update sys.py

--- professor/utils/sys.py
+++ professor/utils/sys.py
@@ -1,17 +1,10 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
+
 
 import time
 
 import psutil
 
-from YukkiMusic.misc import _boot_
+from professor.misc import _boot_
 
 from .formatters import get_readable_time
 
