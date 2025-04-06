d01fbc677f092adb49a0f6288b562cad8f20b396
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2025-04-06 10:02:30 +0530

    Update autoend.py

--- professor/plugins/sudo/autoend.py
+++ professor/plugins/sudo/autoend.py
@@ -1,17 +1,9 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 
 from strings import command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import autoend_off, autoend_on
+from professor import app
+from professor.misc import SUDOERS
+from professor.utils.database import autoend_off, autoend_on
 
 
 @app.on_message(command("AUTOEND_COMMAND") & SUDOERS)
