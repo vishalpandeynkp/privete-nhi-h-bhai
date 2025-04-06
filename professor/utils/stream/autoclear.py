d6d712af83a791347f83bf747f2f95322c2e08db
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2025-04-06 15:31:28 +0530

    Update autoclear.py

--- professor/utils/stream/autoclear.py
+++ professor/utils/stream/autoclear.py
@@ -1,17 +1,8 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 
 import os
 
 from config import autoclean
-from YukkiMusic.utils.decorators import asyncify
+from professor.utils.decorators import asyncify
 
 
 @asyncify
