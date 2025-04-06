845ac48dfc58987e86aab0306b4d523010579f29
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2025-04-06 10:19:32 +0530

    Update speedtest.py

--- professor/plugins/tools/speedtest.py
+++ professor/plugins/tools/speedtest.py
@@ -1,17 +1,9 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 import speedtest
 
 from strings import command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
+from professor import app
+from professor.misc import SUDOERS
 
 
 async def testspeed(m):
