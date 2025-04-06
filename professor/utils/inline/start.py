7d0fbcaa1167fd59325a4a8e5701e7f5110a4e62
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2025-04-06 15:30:30 +0530

    Update start.py

--- professor/utils/inline/start.py
+++ professor/utils/inline/start.py
@@ -1,17 +1,9 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 
 from pyrogram.types import InlineKeyboardButton
 
 from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
-from YukkiMusic import app
+from professor import app
 
 
 def start_pannel(_):
