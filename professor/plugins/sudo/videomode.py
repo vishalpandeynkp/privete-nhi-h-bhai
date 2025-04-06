0b3dc6bd4944d44924c0eb0770b1713e51215954
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2025-04-06 10:10:39 +0530

    Update videomode.py

--- professor/plugins/sudo/videomode.py
+++ professor/plugins/sudo/videomode.py
@@ -1,21 +1,13 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 
 from pyrogram.types import Message
 
 import config
 from strings import command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_off, add_on
-from YukkiMusic.utils.decorators.language import language
+from professor import app
+from professor.misc import SUDOERS
+from professor.utils.database import add_off, add_on
+from professor.utils.decorators.language import language
 
 
 @app.on_message(command("VIDEOMODE_COMMAND") & SUDOERS)
