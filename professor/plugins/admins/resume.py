6f09801efe7c9f352faec450d93f1077a33fdbb1
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2025-04-06 09:24:44 +0530

    Update resume.py

--- professor/plugins/admins/resume.py
+++ professor/plugins/admins/resume.py
@@ -1,22 +1,14 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+
 
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_music_playing, music_on
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from professor import app
+from professor.core.call import Yukki
+from professor.utils.database import is_music_playing, music_on
+from professor.utils.decorators import AdminRightsCheck
 
 
 @app.on_message(command("RESUME_COMMAND") & filters.group & ~BANNED_USERS)
