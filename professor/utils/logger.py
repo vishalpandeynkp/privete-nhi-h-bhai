afd681451f0db4e44d40ca9d21123df017e2026e
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2025-04-06 17:29:32 +0530

    Update logger.py

--- professor/utils/logger.py
+++ professor/utils/logger.py
@@ -1,14 +1,7 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
+
 from config import LOG, LOG_GROUP_ID
-from YukkiMusic import app
-from YukkiMusic.utils.database import is_on_off
+from professor import app
+from professor.utils.database import is_on_off
 
 
 async def play_logs(message, streamtype):
