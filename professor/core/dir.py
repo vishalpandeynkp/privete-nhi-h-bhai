8a44b39145d556ebe8f67fa4021128b0770926c2
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2025-04-06 09:01:54 +0530

    Update dir.py

--- professor/core/dir.py
+++ professor/core/dir.py
@@ -1,11 +1,4 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the "MIT License".
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
-# All rights reserved.
+ All rights reserved.
 #
 import logging
 import os
